bba83f00b8644c452b9f8319374e4f1a8aa8dc3e
sajjan <sajjanmalla619@gmail.com>
2024-02-03 22:37:20 +0545

    initial commit

---
+++ help.c
@@ -0,0 +1,16 @@
+void help()
+{
+    system("cls");
+    printf("\n-------Welcome to QUIZ GAME-------");
+    printf("\n-------How To Play-------");
+    printf("\n___________________________");
+    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the right option.");
+    printf("\n >> Each question carries two marks.");
+    printf("\n >> Negative marking for wrong answers, one mark will be deduct!");
+    printf("\n\n\t!!!!!!!!!!!!! ALL THE BEST !!!!!!!!!!!!!");
+    printf("\n\n\nPress Y to start the game!\n");
+    if ((getch()) == 'y')
+    {
+        ;
+    }
+}
